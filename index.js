293cdda92c2a0505ac16ad023d29a7e0e19c58c7
royabhi647 <royabhi647@gmail.com>
2022-02-10 10:20:50 +0530

    basic js

---
+++ index.js
@@ -0,0 +1,70 @@
+/*
+var a;
+a=10;
+console.log(a);
+console.log(typeof a);
+
+a="hello";
+console.log(a);
+console.log(typeof a);
+
+a=true;
+console.log(a);
+console.log(typeof a);
+
+a=null;
+console.log(a);
+console.log(typeof a);
+
+var num=10;
+console.log(num);
+console.log(typeof num);
+
+var float=2.4;
+console.log(float);
+console.log(typeof float);
+
+var t=true;
+console.log(t);
+console.log(typeof t);
+var f=false;
+console.log(f);
+console.log(typeof f);
+
+var str='a';
+console.log(typeof str);
+
+var b=`hi hope you guys are
+able grasp the concepts`;
+console.log(b);
+
+var num=4500;
+console.log(`half of ${num} is ${num/2}`);
+
+var r="hello";
+console.log(r);
+var r=100;
+console.log(r); */
+
+let l=100;
+console.log(l);
+
+//let l=200;
+//console.log(l);
+l=300;
+console.log(l);
+
+var num=10;
+for(var i=1;i<=num;i++){
+    if(i%2==0){
+        console.log("num is even");
+    }
+}
+
+const a=2;
+console.log(a);
+
+//a=4;
+//console.log(a);
+
+//const a=5;
